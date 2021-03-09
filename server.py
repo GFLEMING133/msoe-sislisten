4bf2025179f0244997940f8617c903fecfb2b593
andy-wojciechowski <wojciechowskia@msoe.edu>
2021-03-08 19:48:44 -0600

    Adding in requirements file and waitress for serving on production

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from waitress import serve
+import os
 
 app = Flask(__name__)
 
@@ -8,5 +10,8 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
-
+    flask_env = os.environ.get('FLASK_ENV')
+    if flask_env is not None and flask_env.lower() == 'production':
+        serve(app, host='127.0.0.1', port=5000, threads=6)
+    else:
+        app.run(debug=True)
